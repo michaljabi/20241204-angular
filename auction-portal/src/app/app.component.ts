e9d1c976260369b8649c80bed2fcd55ad7e4cd19
Jabi <michal.jabi@gmail.com>
2024-12-04 14:39:25 +0100

    task6(AppComponent): turned in to SFC (Single File Component)

--- auction-portal/src/app/app.component.ts
+++ auction-portal/src/app/app.component.ts
@@ -4,8 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    HELLO WORLD... {{title}}
+    <h1 class="bg-warning">Auction Portal</h1>
+    <router-outlet />
+  `,
+  styles: [``]
 })
 export class AppComponent {
   title = 'auction-portal';
