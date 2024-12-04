ddd49532bc616a30e71571cb6307874c8e67df63
Jabi <michal.jabi@gmail.com>
2024-12-04 14:24:53 +0100

    task5(auction-portal): BREAKING - add bootstrap to project.

--- auction-portal/src/main.ts
+++ auction-portal/src/main.ts
@@ -2,5 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// Tak zadziała, bo i tak zbierze to Bundler (Webpack)
+// import 'bootstrap/dist/css/bootstrap.css'
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
