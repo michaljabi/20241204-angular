ea31664853e0d946a0f76392528b29735da40312
Jabi <michal.jabi@gmail.com>
2024-12-06 11:07:05 +0100

    task(3.3)[auctions-page]: handle error + loading in

--- auction-portal/src/app/auction/auctions-page/auctions-page.component.ts
+++ auction-portal/src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuctionsService} from '../auctions.service';
 import {AuctionItem} from '../auction-item';
 import {AuctionCardComponent} from '../auction-card/auction-card.component';
+
+// ZAWSZE this. jeśli chcesz odnieść się do pola klasy!
+// const auctionService = {}
 
 @Component({
   imports: [
@@ -14,30 +17,57 @@ import {AuctionCardComponent} from '../auction-card/auction-card.component';
           <app-auction-card [auction]="item" />
         </div>
       } @empty {
-         <div>Brak aukcji</div>
+        @if(isLoading) {
+          <div class="col-12">
+            <div class="alert alert-info"> Poczekaj... ładuję aukcje... </div>
+          </div>
+        }
+        @if(errorMessage) {
+          <div class="col-12">
+            <div class="alert alert-danger"> Nie udało się załadować aukcji 😭 ! {{errorMessage}} </div>
+          </div>
+        }
       }
     </div>
   `,
   styles: ``
 })
-export class AuctionsPageComponent {
+export class AuctionsPageComponent implements OnInit {
 
+  isLoading = true;
+  errorMessage = "";
   auctions: AuctionItem[] = [];
 
   // dostarcz tutaj instancje AuctionService
   constructor(private auctionService: AuctionsService) {
 
-    // Happy Path (co zrobię jeśli strumień zwróci wartość)
-    auctionService.getAll().subscribe(auctions => {
-      console.log(auctions)
-      this.auctions = auctions;
-    })
+    // // Happy Path (co zrobię jeśli strumień zwróci wartość)
+    // auctionService.getAll().subscribe(auctions => {
+    //   console.log(auctions)
+    //   this.auctions = auctions;
+    // })
 
+  }
+
+  ngOnInit(): void {
+    this.loadAuctions()
+  }
+
+  loadAuctions() {
     // All paths: (co zrobię jeśli jest error, jest wartość albo strumień skończył działanie)
-    auctionService.getAll().subscribe({
-      next: auctions => console.log(auctions),
-      error: err => console.log(err),
-      complete: () => console.log('done')
+    this.isLoading = true;
+    this.auctions = [];
+    this.errorMessage = "";
+    this.auctionService.getAll().subscribe({
+      next: auctions => {
+        this.auctions = auctions;
+        this.isLoading = false;
+      },
+      error: err => {
+        this.errorMessage = err.message;
+        this.isLoading = false;
+      },
+      // complete: () => console.log('done')
     })
   }
 }
