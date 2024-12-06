6e8a82204c824352aa33c1f7e11316c571c0f842
Jabi <michal.jabi@gmail.com>
2024-12-06 15:38:48 +0100

    explain(routes): lazy loading (code-splitting).

--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { auctionRoutes } from './auction/auction.routes';
 import { NotFoundPageComponent } from './not-found-page.component';
-import { routes as adviceRoutes } from './advice/advice.routes';
+// Wyrzucamy import żeby mieć lazy loading !!!!
+// import { routes as adviceRoutes } from './advice/advice.routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auctions', pathMatch: 'full' },
   ...auctionRoutes,
-  ...adviceRoutes,
+  // ...adviceRoutes,
+  { path: 'advices', loadChildren: () => import('./advice/advice.routes').then((m) => m.routes) },
   { path: '**', component: NotFoundPageComponent },
 ];
