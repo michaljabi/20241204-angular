e47d469f5a3c3561ecdf0710ad5503d304f2ece0
Jabi <michal.jabi@gmail.com>
2024-12-06 16:13:51 +0100

    RxJS - using streams + async pipe. reading activatedRoute

--- auction-portal/src/app/not-found-page.component.ts
+++ auction-portal/src/app/not-found-page.component.ts
@@ -1,24 +1,60 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, inject, OnDestroy} from '@angular/core';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {AsyncPipe} from '@angular/common';
+import {interval, map, share, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-not-found-page',
   imports: [
-    RouterLink
+    RouterLink,
+    AsyncPipe
   ],
   template: `
     <div>
       <div class="alert alert-warning">
         Niestety 🥹 strona <code>{{pagePath}}</code> nie istnieje
+
+        <div>{{ pathString$ | async  }}<div>
       </div>
       <a routerLink="/" class="btn btn-outline-info">
-        Powrót do 🏠
+        Powrót do 🏠 {{ intervalHot$ | async }}
       </a>
     </div>
   `,
   styles: ``
 })
-export class NotFoundPageComponent {
+export class NotFoundPageComponent implements OnDestroy {
   //todo: show path that is not found
   pagePath = '/hello/ok'
+  // informacje od routera:
+  activatedRoute = inject(ActivatedRoute);
+  pathString$ = this.activatedRoute.url.pipe(map(url => url.join('/')))
+
+
+  // COLD STREAM
+  interval$ = interval(1000)
+  // HOT STREAM
+  intervalHot$ = this.interval$.pipe(share())
+  intervalSub = new Subscription()
+
+  constructor() {
+    this.activatedRoute.url.subscribe(url => {
+      // console.log(url.join('/'))
+      this.pagePath = url.join('/')
+    })
+
+    // this.activatedRoute.params.subscribe(params => {
+    //   console.log(params)
+    // })
+
+    setTimeout(() => {
+      this.intervalSub = this.intervalHot$.subscribe(val => {
+        console.log(val)
+      })
+    }, 7000)
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSub.unsubscribe()
+  }
 }
